4d1c9fcd0ffc7d6c732a146c87c6b2f71e469168
Marwan Mohamed Yousry <marwan@almotahidaeducation.com>
2026-02-16 23:41:07 +0200

    fix sending api

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,6 +2,7 @@ import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+export const dynamic = 'force-dynamic';
 
 export async function POST(req) {
     try {
